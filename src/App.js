7946e9881e3f3933ac7e04bc1153dbabefc8e67b
Franco <franco.rvl91@gmail.com>
2022-03-15 10:27:49 -0300

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import './App.css';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 import baloonLand from './components/BaloonLand';
 import blueOcean from './components/BlueOcean.js';
 import flowersLand from './components/FlowersLand';
@@ -8,22 +15,25 @@ import mountainLake from './components/MountainLake';
 import nightStars from './components/NightStars';
 import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <Router>
-      <div className='containter'>
-        <h1 className='text-center mt-3 mb-3 text-white'>Galería</h1>
-      </div>
-      <Route path='/BaloonLand' component={baloonLand}/>
-      <Route path='/BlueOcean' component={blueOcean}/>
-      <Route path='/FlowersLand' component={flowersLand}/>
-      <Route path='/FogMountain' component={fogMountain}/>
-      <Route path='/MountainLake' component={mountainLake}/>
-      <Route path='/NightStars' component={nightStars}/>
+ render (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <div className='containter'>
+          <h1 className='text-center mt-3 mb-3 text-white'>Galería</h1>
+        </div>
+        <Route path='/BaloonLand' component={baloonLand}/>
+        <Route path='/BlueOcean' component={blueOcean}/>
+        <Route path='/FlowersLand' component={flowersLand}/>
+        <Route path='/FogMountain' component={fogMountain}/>
+        <Route path='/MountainLake' component={mountainLake}/>
+        <Route path='/NightStars' component={nightStars}/>
 
-      <Navigation/>
-    </Router>
-  );
-}
+        <Navigation/>
+      </Routes>
+    </BrowserRouter>
+  )
 
 export default App;
